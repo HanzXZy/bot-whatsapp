f6dadbd9bb74abc14b00feed45b60bbfecd34793
HanzXZy <mhmd.farhan.abdilah@gmail.com>
2025-06-30 19:19:16 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━          📌 Settings Owner 📌━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\\
+global.ownernumber = '6287883319040' // ganti nomor owner
+global.ownername = 'HanzXZy' // ganti nama bot
+global.fother = "ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ ʙʏ ʜᴀɴxᴢʏ"
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━          📌 Settings Bot 📌━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+global.namabot = "HanzXZy-Bot"
+global.baileys = "@whiskeysocket/baileys"
+global.idSaluran = "120363361543340103@newsletter"
+global.linkSaluran = "https://whatsapp.com/channel/0029VavTjHwADTO8RqOz9C2R"
+botNumber = '6287883319040' // nomor bot
+nomorbot = '6287883319040' // ganti nomor bot untuk pairing
+global.thumbreply = "https://files.catbox.moe/q5xt1b.jpg"
+global.version = "0.0.1"
+global.packname = 'HanzXZy-Botz - 2025'
+global.author = '\nWhatsapp - Bot'
+global.foother = 'HanzXZy-Bot - 2025'
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━          📌 Settings Message 📌━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+global.mess = {
+    success: '[ ☑︎ ] *sʏsᴛᴇᴍ*\nsᴜᴄᴄᴇsғᴜʟʟʏ.',
+    admin: '[ ⚠︎ ] *sʏsᴛᴇᴍ*\n︎ᴋʜᴜsᴜs ᴀᴅᴍɪɴ ɢʀᴏᴜᴘ.',
+    botAdmin: '[ ⚠︎ ] *sʏsᴛᴇᴍ*\nғɪᴛᴜʀ ʜᴀɴʏᴀ ʙɪsᴀ ᴋᴇᴛɪᴋᴀ ʙᴏᴛ ᴊᴀᴅɪ ᴀᴅᴍɪɴ.',
+    creator: '[ 🜲 ] *sʏsᴛᴇᴍ*\nғɪᴛᴜʀ ᴋʜᴜsᴜs ᴏᴡɴᴇʀ.',
+    group: '[ ψ ] *sʏsᴛᴇᴍ*\nғɪᴛᴜʀ ᴋʜᴜsᴜs ɢʀᴏᴜᴘ.',
+    private: '[ ☏ ] *sʏsᴛᴇᴍ*\nғɪᴛᴜʀ ᴋʜᴜsᴜs ᴄʜᴀᴛ ᴘʀɪʙᴀᴅɪ.',
+    wait: '[ ♲︎ ] *sʏsᴛᴇᴍ*\nᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...',
+    premium: '[ 🜲 ] *sʏsᴛᴇᴍ*\nғɪᴛᴜʀ ᴋʜᴜsᴜs ᴘʀᴇᴍɪᴜᴍ',
+    endLimit: '[ ∞ ] *sʏsᴛᴇᴍ*\nʟɪᴍɪᴛ ᴀɴᴅᴀ ʜᴀʙɪs, ᴀᴋᴀɴ ᴅɪ ʀᴇsᴇᴛ sᴇᴛᴇʟᴀʜ sᴇʜᴀʀɪ',
+}
+
+
+
+// *** message *** 
+global.closeMsgInterval = 30; // 30 menit. maksimal 60 menit, minimal 1 menit
+global.backMsgInterval = 2; // 2 jam. maksimal 24 jam, minimal 1 jam
